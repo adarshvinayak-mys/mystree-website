c3c0d54e7c74a922daff2928d34d3a9fdf98db88
Adarsh Vinayak <adarshvinayak@gmail.com>
2026-03-04 22:35:40 +0530

    Use mystreelogo.svg in header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-const mystreelogo = '/logo.webp';
+const mystreelogo = '/mystreelogo.svg';
 
 
 export default function Footer() {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
-const mystreelogo = '/logo.webp';
+const mystreelogo = '/mystreelogo.svg';
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
